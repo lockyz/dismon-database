b00e402496629071075dd35201ca9bfe5546da44
Lockyz <lochlanpainter@gmail.com>
2020-02-20 02:37:17 +1100

    Dismon Stuff
    Changed filepath for all Dismon Files

---
+++ pokemon/dismon/types.js
@@ -0,0 +1,28 @@
+var types = [
+    /*
+    Dismon Typings Database 1.0
+        EXAMPLE ITEM
+    {
+             name: "NAME",
+             stAg: "Strong against",
+             wkAg: "Weak Against"
+         },
+          ANY INFORMATION THAT IS INCORRECT NEEDS TO BE CHANGED.
+          DESCRIPTIONS CAN ONLY BE ONE LINE. SEPERATE LINES WITH \n
+          All unused Data needs to be removed.
+          All the text before the { on each item needs to be removed.
+          Needs to have a command created in the bot to access it.
+          More information will be added if necessary
+      */
+     {
+         name: "Normal",
+         stAg: "EXAMPLE",
+         wkAg: "EXAMPLE"
+     },
+     {
+        name: "Normal10",
+        stAg: "EXAMPLE",
+        wkAg: "EXAMPLE"
+     }
+];
+module.exports = types;
